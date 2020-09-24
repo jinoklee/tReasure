b0eb007a81545d6aa4a639fb6396354c9b278bca
jinoklee <jinoklee.01@gmail.com>
2020-09-24 11:55:43 +0900

    Update shortcut_install.bat

--- shortcut_install.bat
+++ shortcut_install.bat
@@ -1,9 +1,18 @@
 @echo off
+
+set src=c:\Program Files
+set search=Rscript.exe
+
+for /r "%src%" %%F in (*%search%*) do (
+  set full=%%~fF
+  set name=%%~nxF
+)
+
 @Set "SCRIPT=%TEMP%\LinkMaker-%RANDOM%-%RANDOM%.vbs"
 @(  echo Set oWS = WScript.CreateObject("WScript.Shell"^)
     echo sLinkFile = "%USERPROFILE%\Desktop\tReasure.lnk"
     echo Set oLink = oWS.CreateShortcut(sLinkFile^)
-    echo oLink.TargetPath = "C:\Program Files\R\R-4.0.2\Rscript.exe"
+    echo oLink.TargetPath = "%full%"
     echo oLink.Arguments = """%USERPROFILE%\Documents\tReasure.R"""
     echo oLink.IconLocation = "%USERPROFILE%\Downloads\treasure.ico"
     echo oLink.WorkingDirectory = "%USERPROFILE%\Documents"
