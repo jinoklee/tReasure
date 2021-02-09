ddcd2579882b7f7516e9b681dce25c85e4d23765
jinoklee <jinoklee.01@gmail.com>
2021-02-09 16:49:26 +0900

    upload install_win

---
+++ inst/extdata/func/install_win_v1.0.bat
@@ -0,0 +1,64 @@
+:: Require files
+:: pkg_lib.R
+:: loadR_v1.R
+:: tReasure.R
+:: tReasure_source.tar.gz
+
+
+@Echo Off
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: move files on Documents
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+mkdir %USERPROFILE%\Documents\tReasure_v1
+copy * %USERPROFILE%\Documents\tReasure_v1
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: set the path Rscript.exe 
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+set "src=c:\Program Files"
+set "search=Rscript.exe"
+
+for /r "%src%" %%F in (*%search%*) do (
+  set "full=%%~fF"
+)
+echo %full%
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: install the required package for tReasure
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+for /r "%USERPROFILE%\Documents\tReasure_v1" %%F in (*pkg_lib.R*) do (
+ echo %%~fF
+ set "pkg=%%~fF"
+ "%full%" "%%~fF" %*
+)
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: install the tReasure packages
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+for /r "%USERPROFILE%\Documents\tReasure_v1" %%F in (*loadR_v1.R*) do (
+ echo %%~fF
+ set "load=%%~fF"
+ "%full%" "%%~fF" %*
+)
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: set the .lnk on Desktop for tReasure 
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+set lnk=%userprofile%\desktop\tReasure.lnk
+
+set command=^
+$objshell = New-object -ComObject WScript.Shell;^
+$olnk =$objshell.CreateShortcut('%lnk%');^
+$olnk.TargetPath = '%full%';^
+$olnk.Arguments = '%USERPROFILE%\Documents\tReasure_v1\tReasure.R';^
+$olnk.IconLocation = '%USERPROFILE%\Documents\tReasure_v1\tReasure.ico';^
+$olnk.Save();
+
+powershell -command "& {%command%}"
+
+
+
+
