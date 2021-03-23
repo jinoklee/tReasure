06501fd9a3e7544510318a93479bc8dc1f1d5a5a
jinoklee <jinoklee.01@gmail.com>
2021-03-23 23:16:49 +0900

    delete

--- inst/extdata/func/loadR_v1.R
+++
@@ -1,8 +0,0 @@
-
-df <- c()
-for (x in list.files(path =Sys.getenv("USERPROFILE") , pattern="tReasure_0.3.1.tar", recursive=TRUE, full.names =TRUE )) {
-  print(paste("install", x, sep = " "))
-}
-
-
-install.packages(x, repos = NULL, type = "source")
