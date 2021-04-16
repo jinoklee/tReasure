d3df209c7888283aae9caeaa953824818a21d546
jinoklee <jinoklee.01@gmail.com>
2021-04-16 11:36:49 +0900

    update

--- R/tReasure.R
+++ R/tReasure.R
@@ -19,7 +19,7 @@ tReasure <- function(){
   # function
   #......................................................................................#
   source(system.file("extdata", "func/stopFuture.R", package = "tReasure", mustWork = TRUE))
-  source(system.file("extdata", "func/mk_sample.R", package = "tReasure", mustWork = TRUE))
+  #source(system.file("extdata", "func/mk_sample.R", package = "tReasure", mustWork = TRUE))
   source(system.file("extdata", "func/sel_sample.R", package = "tReasure", mustWork = TRUE))
   source(system.file("extdata", "func/anl_trim.R", package = "tReasure", mustWork = TRUE))
   source(system.file("extdata", "func/anl_align.R", package = "tReasure", mustWork = TRUE))
@@ -33,6 +33,44 @@ tReasure <- function(){
   source(system.file("extdata", "func/pyramid.R", package = "tReasure", mustWork = TRUE))
   source(system.file("extdata", "func/volcano.R", package = "tReasure", mustWork = TRUE))
   source(system.file("extdata", "func/barplot.R", package = "tReasure", mustWork = TRUE))
+
+  mk_sample <- function(h,...){
+    dir <- svalue(fq_dir)
+    if(identical(dir,character(0))){
+      gmessage("Warning : Select a directory of FASTQ files")
+    }else{
+      fq_list <- list.files(svalue(fq_dir), pattern = "fastq", full=TRUE)
+      if(length(grep("trim",fq_list)) == 0){
+        sfile <- list.files(svalue(fq_dir), pattern = "fastq")
+      }else{
+        fq_list <- fq_list[-grep("trim", fq_list)]
+        sfile <- list.files(svalue(fq_dir), pattern = "fastq")
+        sfile <- sfile[-grep("trim", sfile)]
+      }
+
+      sname <- gsub(paste(".","fastq", sep = ""), "", sfile)
+      sample <- data.frame(FileName = as.character(fq_list),
+                           SampleName = as.character(sname),
+                           Group = c("control", "test",rep("NA", (length(fq_list)-2))),
+                           Batch = c(rep("NA", length(fq_list))))
+      sample$Group <- as.factor(sample$Group)
+      sample$Batch <- as.character(sample$Batch)
+      tbl[] <- sample
+
+      SampleFile <- file.path(dir, "sample.txt")
+      write.table(sample, SampleFile, sep = "\t", quote = FALSE, row.names = FALSE)
+      if(length(grep("control", sample$Group)) < 2 | length(grep("test", sample$Group)) < 2){
+        insert(st, "Warnning : There should be at least 2 samples per group for differentially expression analysis.")}
+
+      addHandlerChanged(tbl, handler = function(h, ...){
+        new_sample <- tbl[]
+        SampleFile <- file.path(svalue(fq_dir), "sample.txt")
+        write.table(new_sample, SampleFile, sep = "\t", quote = FALSE, row.names = FALSE)
+        insert(st, " ", do.newline = TRUE)
+      })
+      insert(st, ".", do.newline = TRUE)
+    }
+  }
 
   #-------------------------------------------------------------------------------------
   #  tReasure
@@ -123,43 +161,7 @@ tReasure <- function(){
       dir.create(paste(dir, "/rc", sep = ""), recursive = TRUE)}
   })
 
-  mk_sample <- function(h,...){
-    dir <- svalue(fq_dir)
-    if(identical(dir,character(0))){
-      gmessage("Warning : Select a directory of FASTQ files")
-    }else{
-      fq_list <- list.files(svalue(fq_dir), pattern = "fastq", full=TRUE)
-      if(length(grep("trim",fq_list)) == 0){
-        sfile <- list.files(svalue(fq_dir), pattern = "fastq")
-      }else{
-        fq_list <- fq_list[-grep("trim", fq_list)]
-        sfile <- list.files(svalue(fq_dir), pattern = "fastq")
-        sfile <- sfile[-grep("trim", sfile)]
-      }
 
-      sname <- gsub(paste(".","fastq", sep = ""), "", sfile)
-      sample <- data.frame(FileName = as.character(fq_list),
-                           SampleName = as.character(sname),
-                           Group = c("control", "test",rep("NA", (length(fq_list)-2))),
-                           Batch = c(rep("NA", length(fq_list))))
-      sample$Group <- as.factor(sample$Group)
-      sample$Batch <- as.character(sample$Batch)
-      tbl[] <- sample
-
-      SampleFile <- file.path(dir, "sample.txt")
-      write.table(sample, SampleFile, sep = "\t", quote = FALSE, row.names = FALSE)
-      if(length(grep("control", sample$Group)) < 2 | length(grep("test", sample$Group)) < 2){
-        insert(st, "Warnning : There should be at least 2 samples per group for differentially expression analysis.")}
-
-      addHandlerChanged(tbl, handler = function(h, ...){
-        new_sample <- tbl[]
-        SampleFile <- file.path(svalue(fq_dir), "sample.txt")
-        write.table(new_sample, SampleFile, sep = "\t", quote = FALSE, row.names = FALSE)
-        insert(st, " ", do.newline = TRUE)
-      })
-      insert(st, ".", do.newline = TRUE)
-    }
-  }
 
   #-------------------------------------------------------------------------------------
   #  gr2. Quality Control
@@ -292,15 +294,6 @@ tReasure <- function(){
   selaa_button <- gfilebrowse(text = "Isoacceptor level", quote = FALSE, type = "open", container = tmp.32,
                               filter=list("*.txt" = list(patterns = c("*.txt")), "*.*" = list(patterns = c("*"))))
 
-
-  # addHandlerChanged(selfq_button, handler = function(h,...){
-  #   setwd(svalue(selfq_button))
-  #   dir <- getwd()
-  #   if(!dir.exists(paste(dir, "/pre", sep = ""))){
-  #     dir.create(paste(dir, "/pre", sep = ""), recursive = TRUE)}
-  #   if(!dir.exists(paste(dir, "/stat/plot", sep = ""))){
-  #     dir.create(paste(dir, "/stat/plot", sep = ""), recursive = TRUE)}
-  # })
 
   addHandlerChanged(seltrn_button, handler = function(h,...){
     tcount <- read.delim(svalue(seltrn_button))
